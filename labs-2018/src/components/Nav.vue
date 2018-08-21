6bc82776b8519511de936a1bde362b12e2e4bffe
David Gauntlett <david.m.gauntlett@gmail.com>
2018-08-20 20:55:23 -0400

    Init vue for rapid development

---
+++ labs-2018/src/components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title><img src="../assets/maglabs2018.png"></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn flat>FAQ</v-btn>
+    <v-btn flat>Book Hotel</v-btn>
+    <v-btn flat>Register Now</v-btn>
+  </v-toolbar>
+</template>
---
+++ labs-2018/src/main.js
@@ -0,0 +1,18 @@
+import '@babel/polyfill'
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
+
+
+Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
